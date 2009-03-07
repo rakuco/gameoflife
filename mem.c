73c3cfb169f0e8ae957649adbafaf8a89f205345
Raphael Kubo da Costa <kubito@gmail.com>
2009-03-07 20:36:28 -0300

    Set the allocated memory to zero

--- mem.c
+++ mem.c
@@ -18,12 +18,15 @@
 #include <assert.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <string.h>
 
 void* __memAllocate(size_t numelem, size_t elemsize)
 {
   void* chunk = calloc(numelem, elemsize);
 
   assert(chunk != NULL);
+
+  memset(chunk, 0, elemsize);
 
   return chunk;
 }
