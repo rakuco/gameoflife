af94d7f4bab071312b00fe0567e9d100e69a70b5
Raphael Kubo da Costa <kubito@gmail.com>
2009-03-09 00:10:35 -0300

    Doxyfied config.h

--- config.h
+++ config.h
@@ -28,13 +28,38 @@
 
 #define CLI_ARGC 3
 
+/**
+ * Structure to hold the game options.
+ */
 typedef struct {
-  size_t generations;
-  FILE *input_file;
+  size_t generations; /**< Number of generations for which to run the game. */
+  FILE *input_file;   /**< The file with the seed board. */
 } GameConfig;
 
+/**
+ * Frees memory allocated for a GameConfig structure.
+ *
+ * @param config Pointer to a GameConfig structure.
+ */
 void game_config_free(GameConfig *config);
+
+/**
+ * Returns the number of generations for which to run the game.
+ *
+ * @param config Pointer to a GameConfig structure.
+ *
+ * @return The number of generations for which to run.
+ */
 size_t game_config_get_generations(GameConfig *config);
+
+/**
+ * Parses the command line and create a new GameConfig from it.
+ *
+ * @param argc Number of command line arguments.
+ * @param argv Array of command line arguments.
+ *
+ * @return A new GameConfig pointer.
+ */
 GameConfig *game_config_new_from_cli(int argc, char *argv[]);
 
 #endif
